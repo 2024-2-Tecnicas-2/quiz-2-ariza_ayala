cd1c88570ff53301bc649c089d04d96062cecf73
Yakie <hola@gmail.com>
2024-09-30 20:50:07 -0500

    Actualizando

--- src/main/java/com/mycompany/quiz2/Lancha.java
+++ src/main/java/com/mycompany/quiz2/Lancha.java
@@ -3,4 +3,27 @@ package com.mycompany.quiz2;
 
 public class Lancha extends VehiculoBase {
     
+    public TipoCombustible combustible;
+
+    public Lancha( String marca, String modelo, TipoCombustible combustible) {
+        super(marca, modelo);
+        this.combustible = combustible;
+    }
+    
+    
+
+
+    
+    @Override
+    public TipoCombustible tipoCombustible() {
+        return combustible;
+    }
+    
+    
+
+
+
+    
+    
+    
 }
--- src/main/java/com/mycompany/quiz2/Principal.java
+++ src/main/java/com/mycompany/quiz2/Principal.java
@@ -3,6 +3,20 @@ package com.mycompany.quiz2;
 public class Principal {
 
     public static void main(String[] args) {
-}
+        // Crear instancias de Coche y Lancha
+        Coche coche = new Coche("Toyota", "Corolla", TipoCombustible.GASOLINA);
+        Lancha lancha = new Lancha("Yamaha", "242X", TipoCombustible.ELECTRICO);
 
+        // Acelerar y frenar el coche
+        System.out.println("Coche:");
+        coche.acelerar();
+        coche.frenar();
+        System.out.println("Tipo de combustible: " + coche.tipoCombustible());
+
+        // Acelerar y frenar la lancha
+        System.out.println("\nLancha:");
+        lancha.acelerar();
+        lancha.frenar();
+        System.out.println("Tipo de combustible: " + lancha.tipoCombustible());
+    }
 }
